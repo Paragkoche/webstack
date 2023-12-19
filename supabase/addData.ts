c8d123594305dd665fadbaca3f0dd05d85877644
paragkoche <paragkoche0@gmail.com>
2023-12-19 18:26:15 +0530

    update su

---
+++ supabase/addData.ts
@@ -0,0 +1,37 @@
+import { supabase } from "./client";
+
+export const addData = async (data: any) => {
+  const {
+    name,
+    contactNumber,
+    hoursOfWeek,
+    timeSlot,
+  }: { [x in string]: string } = data;
+
+  if (name == "") return new Error("[error] please enter valid name");
+  if (contactNumber == "")
+    return new Error("[error] please enter Contact Number");
+  if (contactNumber != "") {
+    if (!contactNumber.startsWith("+"))
+      return new Error(
+        "[error] please enter valid Contact Number with country code"
+      );
+  }
+  if (hoursOfWeek == "")
+    return new Error(
+      "[error] please enter many hours per week can you commit to the internship?"
+    );
+  if (hoursOfWeek != "") {
+    if (parseInt(hoursOfWeek) > 0) {
+      return new Error("[error] 0 hours is not valid");
+    }
+    if (parseInt(hoursOfWeek) < 168) {
+      return new Error("[error] 1 - 168 hours is not valid");
+    }
+  }
+  if (timeSlot == "")
+    return new Error(
+      "[error] please enter your preferred time slots for the internship."
+    );
+  return await supabase.from("entrys").insert(data);
+};
---
+++ supabase/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient(
+  "https://jsytgpkbwanwjfnqkjxs.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzeXRncGtid2Fud2pmbnFranhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5ODkzOTgsImV4cCI6MjAxODU2NTM5OH0.HKKLkujwUMCY9BNMzvRlPMSisJpzQTjdwFsovPkONS4"
+);
