5d0a75553c72f7c039052a9b24d13604a90d8af3
Parag koche <60149545+Paragkoche@users.noreply.github.com>
2025-05-25 20:36:16 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/contact-us", "/internship-training-program", "/auth"],
-});
+
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
